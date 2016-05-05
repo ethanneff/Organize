af6915b3ad0d646681ad8b9115d7637e0aa015d4
Ethan Neff <ethan.neff@eneff.com>
2016-05-05 12:10:59 -0700

    added coming toon to search

--- Organize/SearchViewController.swift
+++ Organize/SearchViewController.swift
@@ -7,45 +7,20 @@ class SearchViewController: UIViewController {
     setupView()
   }
   
-  override func viewDidLoad() {
-    super.viewDidLoad()
-  }
-  
-  override func didReceiveMemoryWarning() {
-    super.didReceiveMemoryWarning()
-  }
-  
   func setupView() {
     view.backgroundColor = Config.colorBackground
     
-    var label = UILabel(frame: CGRectMake(0,0,50,50))
-    label.backgroundColor = .greenColor()
-    label.text = "hello"
-    view.addSubview(label)
-    
-    label = UILabel()
-    label.text = "hello"
-    view.addSubview(label)
-    
+    let label = UILabel()
+    label.text = "coming soon.."
+    label.textAlignment = .Center
     label.translatesAutoresizingMaskIntoConstraints = false
-    label.leadingAnchor.constraintEqualToAnchor(view.leadingAnchor).active = true
-    label.widthAnchor.constraintEqualToAnchor(view.widthAnchor).active = true
-    label.heightAnchor.constraintEqualToConstant(50).active = true
-    label.bottomAnchor.constraintEqualToAnchor(view.bottomAnchor).active = true
-    label.backgroundColor = .brownColor()
+    view.addSubview(label)
     
-    
-    let button = UIButton(frame: CGRectMake(100,100,100,200))
-    button.setTitle("local notification", forState: .Normal)
-    button.setTitleColor(.blackColor(), forState: .Normal)
-    button.backgroundColor = .yellowColor()
-    button.addTarget(self, action: #selector(buttonPressed(_:)), forControlEvents: .TouchUpInside)
-    view.addSubview(button)
+    NSLayoutConstraint.activateConstraints([
+      label.leadingAnchor.constraintEqualToAnchor(view.leadingAnchor),
+      label.trailingAnchor.constraintEqualToAnchor(view.trailingAnchor),
+      label.heightAnchor.constraintEqualToConstant(Config.buttonHeight),
+      label.centerYAnchor.constraintEqualToAnchor(view.centerYAnchor),
+      ])
   }
-  
-  func buttonPressed(sender: UIButton) {
-
-  }
-  
-
 }
