3bf9310736fd8b216831d0799f622f7a31f61b14
Ethan Neff <ethan.neff@eneff.com>
2016-05-28 08:54:29 -0700

    starting class for firebase

---
+++ Organize/Remote.swift
@@ -0,0 +1,14 @@
+//
+//  Remote.swift
+//  Organize
+//
+//  Created by Ethan Neff on 5/28/16.
+//  Copyright © 2016 Ethan Neff. All rights reserved.
+//
+
+import Foundation
+
+
+class Remote {
+  static let sharedInstance = Remote()
+}
